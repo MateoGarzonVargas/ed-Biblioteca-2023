5aa55ef265309a85a737ef4d049e9c17fc19eeb7
Oscar Mateo Garzón Vargas <148572220+MateoGarzonVargas@users.noreply.github.com>
2023-11-07 01:29:28 +0100

    Add files via upload

---
+++ Prestamo.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+public class Prestamo {
+
+    private String codigo;
+
+    private String fechaPrestamos;
+
+    private String fechaDevolucion;
+
+    private Usuario usuario;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getFechaPrestamos() {
+        return fechaPrestamos;
+    }
+
+    public void setFechaPrestamos(String fechaPrestamos) {
+        this.fechaPrestamos = fechaPrestamos;
+    }
+
+    public String getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(String fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+}
